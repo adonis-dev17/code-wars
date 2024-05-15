f86b17037f00a5c4af66ca821f184e315446fc64
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-15 13:03:30 +0800

    Add number 2. Square sum problem.

--- js/main.js
+++ js/main.js
@@ -27,3 +27,32 @@ const numberToString = num => `${num}`;
 function numberToString(num) {
   return ''+num;
 }
+
+// 2. DESCRIPTION:
+// Complete the square sum function so that it squares each number passed into it and then sums the results together.
+
+// For example, for [1, 2, 2] it should return 9.
+
+// My Solution: 
+function squareSum(numbers){
+  return numbers.reduce((sum,num) => sum + (num * num), 0);
+}
+
+// Other Solutions: 
+function squareSum(numbers){
+  return numbers.reduce(function(sum, n){
+    return (n*n) + sum;
+  }, 0)
+}
+
+// 
+function squareSum(numbers){
+  var result = 0;
+  for (var i = 0; i < numbers.length; i++) {
+    result += (numbers[i] * numbers[i]);
+  }
+  return result;
+}
+
+// 
+const squareSum = numbers => numbers.reduce((a, b)=> a + b**2, 0)
