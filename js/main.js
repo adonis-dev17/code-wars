40ec62fb882a9c1307b8f5beb95c2ada2d53fa8e
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-17 06:17:16 -0500

    Add problem 7, takes threee arguments and do required operation.

--- js/main.js
+++ js/main.js
@@ -115,3 +115,14 @@ function textSpacer(str1) {
   console.log(newStr);
 }
 textSpacer('Boyish');
+
+// 7. The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+// Examples(Operator, value1, value2) --> output
+
+function takesThreeArguments(operator, value1, value2) {
+  return eval(value1  + operator +  value2);
+}
+
+var result = takesThreeArguments('+', 4, 7);
+console.log(result);
