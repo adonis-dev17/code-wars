12be20388cf8f27d210571813c486c11bb475ff2
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-17 06:26:39 -0500

    Change from eval() to switch method since eval is restricted.

--- js/main.js
+++ js/main.js
@@ -117,11 +117,22 @@ function textSpacer(str1) {
 textSpacer('Boyish');
 
 // 7. The function should take three arguments - operation(string/char), value1(number), value2(number).
-The function should return result of numbers after applying the chosen operation.
+// The function should return result of numbers after applying the chosen operation.
 // Examples(Operator, value1, value2) --> output
 
 function takesThreeArguments(operator, value1, value2) {
-  return eval(value1  + operator +  value2);
+  switch (operator) {
+    case '+':
+      return value1 + value2;
+    case '-':
+      return value1 - value2;
+    case '*':
+      return value1 * value2;
+    case '/':
+      return value1 / value2;
+    default:
+      return 'Inavlid operator';
+  }
 }
 
 var result = takesThreeArguments('+', 4, 7);
