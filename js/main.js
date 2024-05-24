b69cb4000fdeec74c3dd838d43c67dba50d2b253
Adonis Medrocillo <adonis.dev17@gmail.com>
2024-05-24 11:12:56 +0800

    Add Clever solution, using Math.pow.

--- js/main.js
+++ js/main.js
@@ -211,7 +211,14 @@ function powersOfTwo(n) {
 }
 powersOfTwo(0);
 
-
+// Clever Solution
+function powersOfTwo(n){
+  var result = [];
+  for (var i = 0; i <= n; i++) {
+    result.push(Math.pow(2, i));
+  }
+  return result;
+}
 
 
 
